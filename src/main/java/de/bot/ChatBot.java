cd47c9c95181f218e33883f20194601e5e5c6a07
Alex Sahm <sahm.alexander@pwc.com>
2019-11-08 08:38:15 +0100

    renaming

--- src/main/java/de/bot/ChatBot.java
+++ src/main/java/de/bot/ChatBot.java
@@ -2,34 +2,22 @@ package de.bot;
 
 import com.rivescript.Config;
 import com.rivescript.RiveScript;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 
 public class ChatBot {
 
-    private RiveScript bot;
+    private RiveScript riveScript;
 
     public ChatBot() {
-        bot = new RiveScript(Config.utf8());
+        riveScript = new RiveScript(Config.utf8());
         try {
-            bot.loadDirectory(getRivescriptDir());
+            riveScript.loadDirectory(getRivescriptDir());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        bot.sortReplies();
+        riveScript.sortReplies();
     }
 
     private String getRivescriptDir() throws FileNotFoundException {
@@ -38,6 +26,6 @@ public class ChatBot {
 
 
     public String getReply(String msg) {
-        return bot.reply("user", msg);
+        return riveScript.reply("user", msg);
     }
 }
